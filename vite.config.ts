fe9c9f93d3665ff12b1713701ffc808b256bd2fa
gerard79-prog <gerard.louchet@laposte.net>
2025-11-07 01:30:30 +0100

    Enhance Vite config with optimization and server settings
    Added optimization and server configuration settings.

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'firebase/app', 'firebase/firestore', 'chart.js']
+  },
+  server: {
+    fs: {
+      strict: false
+    }
+  },
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/]
+    }
+  }
 })
